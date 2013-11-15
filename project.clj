b06cf4f16631f1a5966ee5072cbc2864965c3764
Sam Neubardt <samneubardt@gmail.com>
2013-11-15 13:54:58 -0500

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pitbull "0.1.0-SNAPSHOT"
+(defproject pitbull "0.1.0"
   :description "Clojure Protocol Buffers"
   :url "https://github.com/samn/pitbull"
   :license {:name "Eclipse Public License"
