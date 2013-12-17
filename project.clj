3d873a257456a8aa8935ac010e5eb6c012f9544d
Sam Neubardt <samneubardt@gmail.com>
2013-12-17 18:12:37 -0500

    Add production profile, criterium dependency
    the production profile has jvm settings that criterium won't complain
    about.

--- project.clj
+++ project.clj
@@ -8,7 +8,10 @@
   :jar-exclusions [#"test/"]
   :uberjar-exclusions [#"test/"]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]] :plugins [[lein-midje "3.1.1"]]}
-            :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+             ;; Add an explicit dependency on Clojure in the production profile for perf tests
+             :production {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :dependencies [[com.google.protobuf/protobuf-java "2.5.0"]
                  [potemkin "0.3.4"]
+                 [criterium "0.4.2" :scope "test"]
                  [midje "1.5.1" :scope "test" :exclusions [org.clojure/clojure]]])
