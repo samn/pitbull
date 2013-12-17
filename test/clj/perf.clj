f066ffd0c7e9c9b10eb608c958ac88d9acb05bd9
Sam Neubardt <samneubardt@gmail.com>
2013-12-17 18:30:34 -0500

    First pass at a simple perf test
    uses criterium to bechmark ProtobufMap against raw Map access and
    raw java Protobuf Message usage.

---
+++ test/clj/perf.clj
@@ -0,0 +1,65 @@
+(ns perf
+  "Basic performance test for pitbul.
+  Run like this: lein with-profile production run -m perf"
+  (:require [pitbull :refer :all]
+            [criterium.core :refer :all]
+            [clojure.java.io :as io])
+  (:import [java.io ByteArrayInputStream])
+  (:gen-class))
+
+(defn read-bytes
+  [file-path]
+  (let [file (io/file file-path)
+        buffer (byte-array (.length file))
+        stream (io/input-stream file)] 
+    (.read stream buffer)
+    buffer))
+
+(defn log-section
+  [message]
+  (println)
+  (println)
+  (println message))
+
+(defn bench-raw-map
+  []
+  (log-section "Benchmarking raw Map access")
+  (bench 
+    (let [raw-map {:iattr 350
+                   :sattr "lock ness"
+                   :bar {:bazs [{:baz "hi"} {:baz "ho"}]}}]
+      (-> raw-map :bar :bazs first :baz))))
+
+(defn bench-pbuf-map
+  []
+  (log-section "Benchmarking ProtobufMap access")
+  (bench 
+    (let [pb-map (map->ProtobufMap com.samn.Test$Foo {:iattr 350
+                                                      :sattr "lock ness"
+                                                      :bar {:bazs [{:baz "hi"} {:baz "ho"}]}})]
+      (-> pb-map :bar :bazs first :baz))))
+
+;; TODO bench raw protobuf operations similar to above
+
+(defn bench-load-pbufmap
+  []
+  (log-section "Benchmarking loading a protobuf from disk with load-protobuf")
+  (let [proto-bytes (read-bytes "test/resources/serialized/test1")]
+    (bench
+      (let [pb-map (bytes->ProtobufMap com.samn.Test$Foo proto-bytes)] 
+        (-> pb-map :bar :bazs first :baz)))))
+
+(defn bench-load-pbuf
+  []
+  (log-section "Benchmarking loading a protobuf from disk with raw protobuf")
+  (let [proto-bytes (read-bytes "test/resources/serialized/test1")]
+    (bench
+      (let [pbuf (com.samn.Test$Foo/parseFrom (ByteArrayInputStream. proto-bytes))] 
+        (.. pbuf (getBar) (getBazsList) (get 0) (getBaz))))))
+
+(defn -main
+  [& args]
+  (bench-raw-map)
+  (bench-pbuf-map)
+  (bench-load-pbufmap)
+  (bench-load-pbuf))
