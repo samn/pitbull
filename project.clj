97e7f82945f395552c6f557da999a3e711fb6d89
Sam Neubardt <samneubardt@gmail.com>
2014-09-13 16:43:09 -0400

    Cleanup profiles to be more concise
    should prevent test dependencies from making their way
    into an uberjar.

--- project.clj
+++ project.clj
@@ -7,15 +7,13 @@
   :java-source-paths ["test/java"]
   :jar-exclusions [#"test/"]
   :uberjar-exclusions [#"test/"]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]
-                   :plugins [[lein-midje "3.1.3"]]}
-             ;; Add an explicit dependency on Clojure in the perf profile for performance tests
-             :perf {:dependencies [[org.clojure/clojure "1.6.0"]]}
+  :profiles {:dev [:test {:dependencies [[org.clojure/clojure "1.6.0"]]}]
+             :test {:plugins [[lein-midje "3.1.3"]]
+                    :dependencies [[midje "1.6.3" :exclusions [org.clojure/clojure]]
+                                   [criterium "0.4.2"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :dependencies [[com.google.protobuf/protobuf-java "2.5.0"]
                  ;; later versions of potemkin don't support Clojure 1.4
                  ;; 1.4 support is required for use with Storm
-                 [potemkin "0.3.4"]
-                 [criterium "0.4.2" :scope "test"]
-                 [midje "1.6.3" :scope "test" :exclusions [org.clojure/clojure]]])
+                 [potemkin "0.3.4"]])
